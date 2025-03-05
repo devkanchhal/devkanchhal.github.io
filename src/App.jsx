dbed49d7524312014a2cd5b62b606b5072cd0203
devkanchhal <devutube47@gmail.com>
2025-03-05 20:27:52 +1100

    added some boilerplate mediacard code to edit l8r

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,23 @@ const App = () => {
           <About />
       </section>
 
-      <hr className="border-t-2 border-gray-200" />
+      <hr className="border-t-2 border-gray-100" />
     
-      <section id="group-projects" className="h-screen flex items-center justify-center bg-gray-100">
-        {/* IF WE WANT SECTION BG COLORS -the sections themselves in here (when we make the files) will be w-[80vw] */}
+      <section id="group-projects" className="h-screen flex items-center justify-center bg-gray-50">
+        {/* IF WE WANT SECTION BG COLORS -the sections themselves in here (when we make the files) will be w-[75vw] */}
         <Group />
       </section>
 
-      <section id="personal-projects" className="h-screen flex items-center justify-center bg-gray-300">
+      <hr className="border-t-2 border-gray-100" />
+
+      <section id="personal-projects" className="h-screen flex items-center justify-center">
         <h2 className="text-3xl font-bold">Personal Projects Section</h2>
+      </section>
+
+      <hr className="border-t-2 border-gray-100" />
+
+      <section id="connect" className="h-screen flex items-center justify-center bg-gray-50">
+        <h2 className="text-3xl font-bold">Let's connect Section</h2>
       </section>
       
     </div>
